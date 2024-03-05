a2905e2c09be6ed133ae80a956812c0a0daae675
Nina B <ngbuscemi@gmail.com>
2024-03-04 22:52:53 -0500

    edited files

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -8,22 +8,27 @@ Category.init(
   {
     // Define columns
     id: {
-      type: Datatypes.INTEGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
 
     category_name: {
-      type: Datatypes.STRING,
+      type: DataTypes.STRING,
       allowNull: false
     }
   },
   {
+    // Sequlize instance
     sequelize,
+    // Disregard timestamps in the model
     timestamps: false,
+    // Table name same as model name
     freezeTableName: true,
+    // Uses underscores instead of camelCase for the column names
     underscored: true,
+    // Sets the model name for indentity
     modelName: 'category',
   }
 );
